0d07beb991abfd03378a060f2d65b97e70891bd5
robie081 <robert.lantus@icloud.com>
2023-06-28 18:23:54 +0300

    started work

--- script.js
+++ script.js
@@ -16,21 +16,15 @@
   // console.log('loaded');
   // toggler.style.display = 'none';
 
-  // bttn.onclick = () => {
+  // document.getElementById('bttn').onclick = () => {
   //   document.getElementById('toggler').classList.toggle('open');
   // }
 
-bttn.addEventListener('click', () => {
+document.getElementById('bttn').addEventListener('click', () => {
   document.getElementById('toggler').classList.toggle('open');  
 });
 
-// bttn.addEventListener('click', () => {
-//   document.getElementById('toggler').classList.toggle('overlay');  
-// });
-
-
-
-  // Font Awesome
+// Font Awesome
 
 // const hamNavBttnContainer = document.querySelector('.nav-button');
 // const hamNavBttn = document.querySelector('.nav-button i');
@@ -48,7 +42,9 @@ bttn.addEventListener('click', () => {
 // const hamNavBttnG = document.querySelector('.nav-button span');
 
 
-// bttn.addEventListener('click', () => {
+// Toggle
+
+// document.getElementById('bttn').addEventListener('click', () => {
 document.querySelector('.nav-button').addEventListener('click', () => {
   const toggleElement = document.getElementById('toggleId');
   if(toggleElement.innerHTML === "menu") {
@@ -59,22 +55,96 @@ document.querySelector('.nav-button').addEventListener('click', () => {
 });
 
 
-let index = 1;
-myShow(index);
 
-function currentSlide(n) {
-  myShow(index = n);
+// Accordion
+// Arrow Functions and 'this' in ES6
+
+const acc = document.getElementsByClassName("accordion");
+console.log(acc);
+
+// let spanmore = document.getElementsByClassName('span')[0];
+// spanmore.style.border = '2px solid plum';
+// let spanless = document.getElementsByClassName('span')[1];
+// spanless.style.border = '2px solid green';
+
+for (let i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    // this.classList.toggle("activation");
+    console.log(acc[i]);
+    acc[i].classList.toggle('activation');
+
+    // let panel = this.nextElementSibling;
+    let panel = acc[i].nextElementSibling;
+
+    // console.log(panel);
+
+    // let span = acc[i].children;
+    // let span = acc[i].children[0];
+    // console.log(span);
+
+    let spanmore = acc[i].children[0];
+    let spanless = acc[i].children[1];
+    console.log(spanmore);
+    console.log(spanless);
+  
+    // spanmore.style.border = '2px solid plum';
+    // spanless.style.border = '2px solid green';
+
+
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+      spanless.classList.add('display');
+      spanmore.classList.remove('display');
+
+    } else {
+      panel.style.display = "block";
+      spanless.classList.remove('display');
+      spanmore.classList.add('display');
+    }
+  });
 }
 
-function myShow(n) {
-  console.log(`n ${n}`);
-  console.log(`index ${index}`);
-  let slides = document.getElementsByClassName("mySlides");
 
-  for (let i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  slides[index - 1].style.display = "block";
-}
+// var acc = document.getElementsByClassName("accordion");
+// var i;
+
+// for (i = 0; i < acc.length; i++) {
+//   acc[i].addEventListener("click", function() {
+//     /* Toggle between adding and removing the "active" class,
+//     to highlight the button that controls the panel */
+//     this.classList.toggle("active");
+
+//     /* Toggle between hiding and showing the active panel */
+//     var panel = this.nextElementSibling;
+//     if (panel.style.display === "block") {
+//       panel.style.display = "none";
+//     } else {
+//       panel.style.display = "block";
+//     }
+//   });
+// }
+
+
+
+
+// Pagination
+
+// let index = 1;
+// myShow(index);
+
+// function currentSlide(n) {
+//   myShow(index = n);
+// }
+
+// function myShow(n) {
+//   console.log(`n ${n}`);
+//   console.log(`index ${index}`);
+//   let slides = document.getElementsByClassName("mySlides");
+
+//   for (let i = 0; i < slides.length; i++) {
+//       slides[i].style.display = "none";
+//   }
+//   slides[index - 1].style.display = "block";
+// }
 
 // }
